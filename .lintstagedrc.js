8de5edf04dc5f8bf5cbceffd02b5af40ec8ff27b
Manuel Moya <mmoyaferrer@gmail.com>
2023-12-06 17:12:52 +0100

    chore: switch to four spaces indent

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,3 +1,3 @@
 module.exports = {
-  '**/*.{auradoc,cls,cmp,component,css,design,html,js,json,md,page,trigger,xml,yaml,yml}': 'prettier --write'
+    '**/*.{auradoc,cls,cmp,component,css,design,html,js,json,md,page,trigger,xml,yaml,yml}': 'prettier --write'
 };
