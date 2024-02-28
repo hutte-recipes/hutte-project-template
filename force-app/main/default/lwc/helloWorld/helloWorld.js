b17426f88355059718717e74538d7d4903c63bc4
Manuel Moya <mmoyaferrer@gmail.com>
2024-02-28 21:00:31 +0100

    chore: some mock changes to test quality scans, this commit will be undone

--- force-app/main/default/lwc/helloWorld/helloWorld.js
+++ force-app/main/default/lwc/helloWorld/helloWorld.js
@@ -1,3 +1,10 @@
 import { LightningElement } from 'lwc';
 
-export default class HelloWorld extends LightningElement {}
+export default class HelloWorld extends LightningElement {
+
+    connectedCallback() {
+        // Vulnerable code snippet
+        var userInput = "<script>alert('XSS vulnerability!');</script>";
+        document.getElementById("output").innerHTML = userInput;
+    }
+}
