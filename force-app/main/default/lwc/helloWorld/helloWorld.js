df8b0e5f06f6bef6c98fb03461de2439a1d81cc2
Manuel Moya <mmoyaferrer@gmail.com>
2024-03-01 10:21:34 +0100

    ci: undo mock changes in metadataa

--- force-app/main/default/lwc/helloWorld/helloWorld.js
+++ force-app/main/default/lwc/helloWorld/helloWorld.js
@@ -1,10 +1,3 @@
 import { LightningElement } from 'lwc';
 
-export default class HelloWorld extends LightningElement {
-
-    connectedCallback() {
-        // Vulnerable code snippet
-        var userInput = "<script>alert('XSS vulnerability!');</script>";
-        document.getElementById("output").innerHTML = userInput;
-    }
-}
+export default class HelloWorld extends LightningElement {}
